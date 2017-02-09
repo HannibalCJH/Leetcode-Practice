b0dab3d7ae4a9e9b9bb244d9318bc7ead04a777f
HannibalCJH <hannibalcjh@gmail.com>
2017-02-09 13:05:41 -0600

    Update C#: BFS Iterative Solution.cs

--- 297. Serialize and Deserialize Binary Tree/C#: BFS Iterative Solution.cs
+++ 297. Serialize and Deserialize Binary Tree/C#: BFS Iterative Solution.cs
@@ -34,41 +34,41 @@ public class Codec {
 
     // Decodes your encoded data to tree.
     public TreeNode deserialize(string data)
-	{
-		if (data == null || data.Length == 0)
-			return null;
-		// 传入的data是"1,1,#,#,"，最后又逗号
+    {
+        if(data == null || data.Length == 0)
+            return null;
+        // 传入的data是"1,1,#,#,"，最后又逗号
         // 如果不加RemoveEmptyEntries则最后会有一个空字符串
-		string[] vals = data.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-		int[] nullNum = new int[vals.Length];
-		TreeNode[] nodes = new TreeNode[vals.Length];
-		// 第一遍记录每个节点之前的空节点个数
-		for (int i = 0; i < vals.Length; i++)
-		{
-			if (i > 0)
-				nullNum[i] = nullNum[i - 1];
+        string[] vals = data.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        int[] nullNum = new int[vals.Length];
+        TreeNode[] nodes = new TreeNode[vals.Length];
+        // 第一遍记录每个节点之前的空节点个数
+        for(int i = 0; i < vals.Length; i++)
+        {
+            if(i > 0)
+                nullNum[i] = nullNum[i - 1];
 
-			if (vals[i].Equals("#"))
-			{
-				nullNum[i]++;
-				nodes[i] = null;
-			}
-			else
-			{
-				nodes[i] = new TreeNode(Convert.ToInt32(vals[i]));
-			}
-		}
+            if(vals[i].Equals("#"))
+            {
+                nullNum[i]++;
+                nodes[i] = null;
+            }
+            else
+            {
+                nodes[i] = new TreeNode(Convert.ToInt32(vals[i]));
+            }
+        }
 
-		// 第二遍重建二叉树
-		for (int i = 0; i < vals.Length; i++)
-		{
-			if (nodes[i] == null)
-				continue;
-			nodes[i].left = nodes[(i - nullNum[i]) * 2 + 1];
-			nodes[i].right = nodes[(i - nullNum[i]) * 2 + 2];
-		}
-		return nodes[0];
-	}
+        // 第二遍重建二叉树
+        for(int i = 0; i < vals.Length; i++)
+        {
+            if (nodes[i] == null)
+                continue;
+            nodes[i].left = nodes[(i - nullNum[i]) * 2 + 1];
+            nodes[i].right = nodes[(i - nullNum[i]) * 2 + 2];
+        }
+        return nodes[0];
+    }
 }
 
 // Your Codec object will be instantiated and called as such:
