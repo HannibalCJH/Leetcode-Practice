3fe482cd53bfb50709196ebe22e7cc061e0fb544
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-09 19:50:27 -0600

    Update Solution.java

--- 311. Sparse Matrix Multiplication/Solution.java
+++ 311. Sparse Matrix Multiplication/Solution.java
@@ -1,46 +1,45 @@
 public class Solution {
-    // 稀疏矩阵表示法：http://www.cs.cmu.edu/~scandal/cacm/node9.html
     // 时间复杂度O(mnh)，空间复杂度O(mh)
     public int[][] multiply(int[][] A, int[][] B) 
     {
-        int m = A.length, n = A[0].length, nB = B[0].length;
-        int[][] result = new int[m][nB];
-        
-        // 用一个List数组来记录矩阵A的非零元素
-        // 每行为一个List，List中先存一个index再存这个index对应的元素
-        List[] indexA = new List[m];
-        for(int i = 0; i < m; i++) 
+        // 两个矩阵相乘，矩阵A的列数必须等于矩阵B的行数
+        // A为m x n，B为n x h，最后相乘的结果C为m x h
+        int m = A.length, n = A[0].length, h = B[0].length;
+        int[][] C = new int[m][h];
+        // 存A矩阵中的非零元素
+        List[] ANums = new List[m];
+        // 把稀疏矩阵A转换成链表
+        for(int i = 0; i < m; i++)
         {
-            List<Integer> numsA = new ArrayList<Integer>();
-            for(int j = 0; j < n; j++) 
+            // 每行都是一个ArrayList
+            ANums[i] = new ArrayList<Integer>();
+            for(int j = 0; j < n; j++)
             {
                 // 非零元素
                 if(A[i][j] != 0)
                 {
-                    numsA.add(j); 
-                    numsA.add(A[i][j]);
+                    // 一次存两个，先存列下标，再存元素
+                    ANums[i].add(j);
+                    ANums[i].add(A[i][j]);
                 }
             }
-            indexA[i] = numsA;
         }
-        
         // 矩阵相乘
-        for(int i = 0; i < m; i++) 
+        for(int i = 0; i < m; i++)
         {
-            List<Integer> numsA = indexA[i];
-            // 每次跳过两个，第一个是index第二个是对应的元素，两个为一组
-            for(int p = 0; p < numsA.size() - 1; p += 2) 
+            // 取出A矩阵第i行的非零元素
+            List<Integer> ACols = ANums[i];
+            // 存的时候是一组存两个，先列下标后元素，所以每次跳两个
+            for(int pos = 0; pos < ACols.size() - 1; pos += 2)
             {
-                int colA = numsA.get(p);
-                int valA = numsA.get(p + 1);
-                for(int j = 0; j < nB; j++) 
+                int col = ACols.get(pos);
+                int valA = ACols.get(pos + 1);
+                for(int j = 0; j < h; j++)
                 {
-                    int valB = B[colA][j];
-                    result[i][j] += valA * valB;
+                    C[i][j] += valA * B[col][j];
                 }
             }
         }
-    
-        return result;   
+        return C;
     }
 }
