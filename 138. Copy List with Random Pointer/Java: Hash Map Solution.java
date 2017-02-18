ca3717c682610ced364b151984c00e95d4c14a5d
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-18 16:39:26 -0600

    Create Java: Hash Map Solution.java

---
+++ 138. Copy List with Random Pointer/Java: Hash Map Solution.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    // 时间复杂度O(n)，空间复杂度O(n)
+    public RandomListNode copyRandomList(RandomListNode head) 
+    {
+        if(head == null)
+            return null;
+        
+        HashMap<RandomListNode, RandomListNode> map = new HashMap<>();
+        RandomListNode node = head;
+        // 复制头节点
+        map.put(node, new RandomListNode(node.label));
+        while(node != null)
+        {
+            // 复制下一个节点
+            map.get(node).next = copyNode(node.next, map);
+            // 复制随机节点
+            map.get(node).random = copyNode(node.random, map);
+            node = node.next;
+        }
+        return map.get(head);
+    }
+    
+    private RandomListNode copyNode(RandomListNode node, HashMap<RandomListNode, RandomListNode> map)
+    {
+        if(!map.containsKey(node))
+        {
+            if(node != null)
+                map.put(node, new RandomListNode(node.label));
+            else
+                map.put(node, null);
+        }
+        return map.get(node);
+    }
+}
