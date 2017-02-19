f1806c1287361d3e16c4cb053473e6796383f418
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-18 20:38:35 -0600

    Create C#: Recursive Solution.cs

---
+++ 236. Lowest Common Ancestor of a Binary Tree/C#: Recursive Solution.cs
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) 
+    {
+        if(root == null || root == p || root == q)
+            return root;
+            
+        TreeNode left = LowestCommonAncestor(root.left, p, q);
+        TreeNode right = LowestCommonAncestor(root.right, p, q);
+        
+        // 找到LCA且不是p或者q
+        if(left != null && right != null)
+            return root;
+        // LCA就是p或者q
+        if(left != null)
+            return left;
+        if(right != null)
+            return right;
+        return null;
+    }
+}
