10855867cf7a53bf9651631b11661f9000ed9520
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-18 18:58:28 -0600

    Create Java: Solution.java

---
+++ 168. Excel Sheet Column Title/Java: Solution.java
@@ -0,0 +1,35 @@
+public class Solution {
+    // 时间复杂度O(n)，空间复杂度O(1)
+    public String convertToTitle(int n) 
+    {
+        StringBuilder sb = new StringBuilder();
+        
+        while(n > 0)
+        {
+            n--;
+            sb.insert(0, (char)(n % 26 + 'A'));
+            n /= 26;
+        }
+        return sb.toString();
+    }
+    
+    // 我的版本
+    public String convertToTitle2(int n) 
+    {
+        StringBuilder sb = new StringBuilder();
+        
+        while(n > 0)
+        {
+            int mod = n % 26;
+            if(mod == 0)
+            {
+                mod = 26;
+                n--;
+            }
+            
+            sb.insert(0, (char)(mod - 1 + 'A'));
+            n /= 26;
+        }
+        return sb.toString();
+    }
+}
