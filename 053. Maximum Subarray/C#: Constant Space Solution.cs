8dff6cf7225861ac521366fae6c8a21ea732d9f1
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-19 17:19:38 -0600

    Create C#: Constant Space Solution.cs

---
+++ 053. Maximum Subarray/C#: Constant Space Solution.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) 
+    {
+        if(nums == null || nums.Length == 0)
+            return 0;
+        int max = int.MinValue;
+        int temp = 0;
+        for(int i = 0; i < nums.Length; i++)
+        {
+            temp = Math.Max(temp + nums[i], nums[i]);
+            max = Math.Max(temp, max);
+        }
+        return max;
+    }
+}
