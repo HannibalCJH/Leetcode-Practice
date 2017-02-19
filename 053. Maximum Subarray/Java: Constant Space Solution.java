8ec1e5279baa483e5365fe893dd1935ecccfbb0a
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-19 17:42:52 -0600

    Update Java: Constant Space Solution.java

--- 053. Maximum Subarray/Java: Constant Space Solution.java
+++ 053. Maximum Subarray/Java: Constant Space Solution.java
@@ -1,19 +1,23 @@
 public class Solution {
     // 时间复杂度O(n)，空间复杂度O(1)
-    public int maxSubArray(int[] A) 
+    public int maxSubArray(int[] nums) 
     {
-	// 记录全局最大值
-        int maxSum = Integer.MIN_VALUE;  
+        if(nums == null || nums.length == 0)
+            return 0;
+        
         // 记录当前最大值
-        int temp = 0;  
-        for(int i = 0; i < A.length; i++)
-        {  
-            // 如果temp+A[i]比A[i]大，那A[i]属于temp形成的子数组
-            // 不然重新开始一个以A[i]开头的子数组
-            temp = Math.max(temp + A[i], A[i]);  
+        int temp = nums[0];
+        // 记录全局最大值
+        int max = temp;
+        
+        for(int i = 1; i < nums.length; i++)
+        {
+            // 如果temp+nums[i]比nums[i]大，那nums[i]属于temp形成的子数组
+            // 不然重新开始一个以nums[i]开头的子数组
+            temp = Math.max(temp + nums[i], nums[i]);
             // 比较全局和当前最大值，如果当前的大则更新为全局最大值
-            maxSum = Math.max(temp, maxSum);  
-        }  
-        return maxSum;  
+            max = Math.max(temp, max);
+        }
+        return max;
     }
 }
