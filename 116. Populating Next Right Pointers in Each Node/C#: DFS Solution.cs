5073e905f542a22e6b5d3680b0c3ecaa2d1c84a2
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-20 20:24:19 -0600

    Create C#: DFS Solution.cs

---
+++ 116. Populating Next Right Pointers in Each Node/C#: DFS Solution.cs
@@ -0,0 +1,29 @@
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     public int val;
+ *     public TreeLinkNode left, right, next;
+ *     public TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    // 每次建立的是当前层的下一层的next节点，可以利用next指针
+    // 时间复杂度O(n)，空间复杂度O(logn)，满二叉树的树高是logn
+    public void Connect(TreeLinkNode root) 
+    {
+        if(root == null)
+            return;
+        // 不是最后一层叶节点
+        if(root.left != null)
+        {
+            // 左节点的next指针指向右节点
+            root.left.next = root.right;
+            // 这一层的next节点已经在上一层的时候建立好
+            if(root.next != null)
+                root.right.next = root.next.left;
+        }
+        
+        Connect(root.left);
+        Connect(root.right);
+    }
+}
