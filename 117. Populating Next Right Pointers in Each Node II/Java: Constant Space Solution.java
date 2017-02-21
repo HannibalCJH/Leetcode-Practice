51f26c551a9f00f9b6aadc71546ebd1deac0018f
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-20 21:12:35 -0600

    Create Java: Constant Space Solution.java

---
+++ 117. Populating Next Right Pointers in Each Node II/Java: Constant Space Solution.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     int val;
+ *     TreeLinkNode left, right, next;
+ *     TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    // 时间复杂度O(n)，空间复杂度O(1)
+    // 下一层的第一个节点
+    TreeLinkNode head = null;
+    // 下一层的前一个节点
+    TreeLinkNode prev = null;
+    
+    public void connect(TreeLinkNode root) 
+    {
+        // 当前层的节点
+        TreeLinkNode cur = root;
+        while(cur != null) 
+        {
+            // 遍历当前层
+            while(cur != null) 
+            {
+                // 左子树
+                checkChild(cur.left);
+                // 右子树
+                checkChild(cur.right);
+                // 移动到当前层的下一个节点
+                cur = cur.next;
+            }
+            
+            // 移动到下一层的第一个节点
+            cur = head;
+            head = null;
+            prev = null;
+        }
+    }
+    
+    private void checkChild(TreeLinkNode node)
+    {
+        // 左子树
+        if(node != null) 
+        {
+            if(prev != null)
+                prev.next = node;
+            else
+                // 如果下一层的上一个节点是空则当前节点是下一层的第一个节点
+                head = node;
+            prev = node;
+        }
+    }
+}
