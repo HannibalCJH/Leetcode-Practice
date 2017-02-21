1b1beab8fc323a1fdb5a1b59e495ac797ce17924
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-20 19:19:20 -0600

    Create C#: BFS Solution.cs

---
+++ 116. Populating Next Right Pointers in Each Node/C#: BFS Solution.cs
@@ -0,0 +1,40 @@
+/**
+ * Definition for binary tree with next pointer.
+ * public class TreeLinkNode {
+ *     public int val;
+ *     public TreeLinkNode left, right, next;
+ *     public TreeLinkNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    // 递归法
+    // 时间复杂度O(n)，空间复杂度O(n)
+    public void Connect(TreeLinkNode root) 
+    {
+        if(root == null)
+            return;
+        Queue<TreeLinkNode> queue = new Queue<TreeLinkNode>();
+        queue.Enqueue(root);
+        while(queue.Count > 0)
+        {
+            // 这一层是叶子节点
+            if(queue.Peek().left == null)
+                break;
+            // 每层开始初始化前一个兄弟节点为空
+            TreeLinkNode pre = null;
+            int size = queue.Count;
+            for(int i = 0; i < size; i++)
+            {
+                TreeLinkNode cur = queue.Dequeue();
+                // 前一个兄弟节点的右节点指向当前节点的左节点
+                if(pre != null)
+                    pre.right.next = cur.left;
+                // 左节点的右指针指向右节点
+                cur.left.next = cur.right;
+                pre = cur;
+                queue.Enqueue(cur.left);
+                queue.Enqueue(cur.right);
+            }
+        }
+    }
+}
