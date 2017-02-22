8881daf8ee3b2fec6fb09eb4726392be514620d5
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-21 22:11:09 -0600

    Update Java: Iterative Solution.java

--- 021. Merge Two Sorted Lists/Java: Iterative Solution.java
+++ 021. Merge Two Sorted Lists/Java: Iterative Solution.java
@@ -8,40 +8,32 @@
  */
 public class Solution {
     // 迭代法
-    // 时间复杂度O(n)，空间复杂度O(1)
+    // 时间复杂度O(min(m, n))，只需遍历完短的链表，然后把长链表余下的直接接到结果后面
+    // 空间复杂度O(1)
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) 
     {
         ListNode dummy = new ListNode(-1);
         ListNode pre = dummy;
         while(l1 != null && l2 != null)
         {
-            if(l1.val > l2.val)
-            {
-                pre.next = l2;
-                l2 = l2.next;
-            }
-            else
+            if(l1.val < l2.val)
             {
                 pre.next = l1;
                 l1 = l1.next;
             }
+            else
+            {
+                pre.next = l2;
+                l2 = l2.next;
+            }
             pre = pre.next;
         }
-        
-        while(l1 != null)
-        {
+        // 直接把剩下没遍历完的那个链表接上就行，因为本来链表就是排序过的
+        if(l1 != null)
             pre.next = l1;
-            l1 = l1.next;
-            pre = pre.next;
-        }
-        
-        while(l2 != null)
-        {
+        if(l2 != null)
             pre.next = l2;
-            l2 = l2.next;
-            pre = pre.next;
-        }
-        
+            
         return dummy.next;
     }
 }
