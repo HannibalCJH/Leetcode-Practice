f4da8972fa83e3bf91925b2f38e807130c9c7df5
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-23 14:11:05 -0600

    Create C#: Solution.cs

---
+++ 026. Remove Duplicates from Sorted Array/C#: Solution.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int RemoveDuplicates(int[] nums) 
+    {
+        if(nums == null || nums.Length == 0)
+            return 0;
+        
+        int index = 1;
+        for(int i = 1; i < nums.Length; i++)
+        {
+            if(nums[i] != nums[i - 1])
+                nums[index++] = nums[i];
+        }
+        return index;
+    }
+}
