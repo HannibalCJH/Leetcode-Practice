a5ca8915461054bcdde6f76af63528d6ba664916
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-22 20:54:13 -0600

    Update Java: Binary Search Solution.java

--- 033. Search in Rotated Sorted Array/Java: Binary Search Solution.java
+++ 033. Search in Rotated Sorted Array/Java: Binary Search Solution.java
@@ -1,36 +1,39 @@
 public class Solution {
+    // 二分搜索
     // 时间复杂度O(logn)，空间复杂度O(1)
     public int search(int[] nums, int target) 
     {
-        if(nums == null || nums.length == 0) 
-			return -1;  
-		
-		// 二分法
-        int left = 0, right = nums.length - 1;  
-        while(left <= right) 
-        {  
-            int mid = left + (right - left) / 2;  
+        if(nums == null || nums.length == 0)
+            return -1;
+            
+        int left = 0, right = nums.length - 1;
+        while(left <= right)
+        {
+            int mid = left + (right - left) / 2;
             // 找到目标
-            if(nums[mid] == target)  
-                return mid;  
-            // 当前半边是正常顺序, 旋转轴心在另一半
-            if(nums[left] <= nums[mid]) 
-            {  
-            	// 目标在这一半中
-                if(nums[left] <= target && target < nums[mid])  
-                    right = mid - 1;  
-                else 
-                    left = mid + 1; 
-            } 
-            // 当前半边是异常顺序, 旋转轴心在这一半
-            else 
-            {  
-                if(nums[mid] < target && target <= nums[right]) 
-                    left = mid + 1;  
-                else  
-                    right = mid - 1;  
-            }  
-        }  
-        return -1; 
+            if(nums[mid] == target)
+                return mid;
+            // 左半边是正常顺序
+            else if(nums[left] <= nums[mid])
+            {
+                // 判断target是不是落在顺序正常的左半边
+                if(nums[left] <= target && nums[mid] > target)
+                    right = mid - 1;
+                else
+                    // 如果不在左半边那只能在右半边
+                    left = mid + 1;
+            }
+            // 左半边是不正常顺序
+            else
+            {
+                // 判断target是不是落在顺序正常的右半边
+                if(nums[right] >= target && target > nums[mid])
+                    left = mid + 1;
+                else
+                    // 如果不在右半边那只能在左半边
+                    right = mid - 1;
+            }
+        }
+        return -1;
     }
 }
