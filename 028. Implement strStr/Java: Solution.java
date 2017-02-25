1d7daf3c2e0ec5edcdaff2bd896e7aaed13240fa
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-25 16:07:35 -0600

    Create Java: Solution.java

---
+++ 028. Implement strStr/Java: Solution.java
@@ -0,0 +1,30 @@
+public class Solution {
+    public int strStr(String haystack, String needle) 
+    {
+        // needle是空字符串的情况返回0
+        if(needle.length() == 0)
+            return 0;
+        int i1 = 0, i2 = 0;
+        // 保证haystack能截取的字符串长度大于等于needle的长度
+        for(int start = 0; start <= haystack.length() - needle.length(); start++)
+        {
+            // haystack第一个字符和needle的第一个字符不一样直接跳过
+            if(haystack.charAt(start) != needle.charAt(0))
+                continue;
+            // 记录haystack开始的位置
+            i1 = start;
+            // needle的指针
+            i2 = 0;
+            // 逐一比较字符
+            while(i2 < needle.length() && haystack.charAt(i1) == needle.charAt(i2))
+            {
+                i1++;
+                i2++;
+            }
+            // 如果截取长度等于needle说明找到了needle
+            if(i2 == needle.length())
+                return start;
+        }
+        return -1;
+    }
+}
