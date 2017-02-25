77c7a0714b7384390748b2ca42af3c7414fe8c7f
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-25 14:11:31 -0600

    Create C#: Solution.cs

---
+++ 258. Add Digits/C#: Solution.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    public int AddDigits(int num) 
+    {
+        while(num > 9)
+        {
+            int sum = 0;
+            while(num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            num = sum;
+        }
+        return num;
+    }
+}
