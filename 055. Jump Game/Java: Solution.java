81ae4f10878d71d56e2be10710e039ebb5184560
Hannibal Chen <hannibalcjh@gmail.com>
2017-02-26 14:51:12 -0600

    Update Java: Solution.java

--- 055. Jump Game/Java: Solution.java
+++ 055. Jump Game/Java: Solution.java
@@ -8,6 +8,7 @@ public class Solution {
         int maxReach = 0;
         for(int i = 0; i < nums.length; i++)
             // 当前的位置还没有到达全局最远的距离，查看从这个点能否跳到比maxReach更远的点
+            // 注意数组中的数不是代表这个位置一定要跳的步数，而是可以跳的最远的步数，你可以跳0到nums[i]之间任意的步数
             if(i <= maxReach)
                 maxReach = Math.max(maxReach, i + nums[i]);
         // 全局最远的点是不是能到达最后一个位置
