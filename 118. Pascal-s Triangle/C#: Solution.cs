a3a0368245a63632d71485dba8d0477138699183
Hannibal Chen <hannibalcjh@gmail.com>
2017-03-06 21:43:44 -0600

    Create C#: Solution.cs

---
+++ 118. Pascal-s Triangle/C#: Solution.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public IList<IList<int>> Generate(int numRows) 
+    {
+        IList<IList<int>> result = new List<IList<int>>();
+        for(int i = 0; i < numRows; i++)
+        {
+            IList<int> cur = new List<int>();
+            for(int j = 0; j < i + 1; j++)
+            {
+                if(j == 0 || j == i)
+                {
+                    cur.Add(1);
+                }
+                else
+                {
+                    IList<int> upLevel = result[i - 1];
+                    int upLeft = upLevel[j - 1], upRight = upLevel[j];
+                    cur.Add(upLeft + upRight);
+                }
+            }
+            result.Add(cur);
+        }
+        return result;
+    }
+}
