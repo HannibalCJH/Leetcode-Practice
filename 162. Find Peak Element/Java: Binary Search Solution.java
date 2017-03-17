ec2f3ef40aff25bb3b103c9ec54f02d368507839
Hannibal Chen <hannibalcjh@gmail.com>
2017-03-17 16:24:54 -0500

    Update Java: Binary Search Solution.java

--- 162. Find Peak Element/Java: Binary Search Solution.java
+++ 162. Find Peak Element/Java: Binary Search Solution.java
@@ -3,18 +3,25 @@ public class Solution {
     public int findPeakElement(int[] nums) 
     {
         int left = 0, right = nums.length - 1;
-        while(left < right)
+        while(left <= right)
         {
-            int mid = left + (right - left) / 2;
-            // 左右边界相邻
-            if(left == mid)
-                return nums[left] < nums[right] ? right : left;
-            // 中点比右边小，则右边是上坡，抛弃左边
+            // 找到峰值
+            if(left == right)
+                return left;
+                
+            int mid = (left + right) / 2;
+            // 中点比右边小，那右半边肯定包含一个峰值
+            // 这里指的右边不包含中点，因为我们比较了中点和它右边的点而且右边的点比较大，那可能的峰值最起码是右边这个大的点
+            // 而不会是比右边点小的中点
             if(nums[mid] < nums[mid + 1])
-                left = mid;
-            // 中点比右边大，则右边是下坡，抛弃右边
-            if(nums[mid] > nums[mid + 1])
+            {
+                left = mid + 1;
+            }
+            else
+            {
+                // 中点比右边大，那中点本身可能是峰值，所以左半边还是要包含中点
                 right = mid;
+            }
         }
         return 0;
     }
