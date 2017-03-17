32bb2991c700df92b5bc993202fa2d431436f37e
Hannibal Chen <hannibalcjh@gmail.com>
2017-03-17 11:26:07 -0500

    Update C#: Constant Space Solution.cs

--- 116. Populating Next Right Pointers in Each Node/C#: Constant Space Solution.cs
+++ 116. Populating Next Right Pointers in Each Node/C#: Constant Space Solution.cs
@@ -12,29 +12,30 @@ public class Solution {
     {
         if(root == null)
             return;
-            
+        
         // 当前层的节点
         TreeLinkNode cur = root;
         // 下一层的第一个节点
-        TreeLinkNode head = root.left;
-        while(cur.left != null)
+        TreeLinkNode nextLevel = cur.left;
+        // 当下一层的节点不为空
+        while(nextLevel != null)
         {
-            while(cur != null)
+            // 当前左节点的next指针指向右节点
+            cur.left.next = cur.right;
+            // 如果当前节点还不是这一层的最后一个节点，
+            // 则当前节点右节点的next指针指向这一层下一个节点的左节点
+            if(cur.next != null)
             {
-                // 设置下一层的第一个节点
-                if(head == null)
-                    head = cur.left;
-                // 当前左节点的next指针指向右节点
-                cur.left.next = cur.right;
-                // 如果当前节点还不是这一层的最后一个节点，
-                // 则当前节点右节点的next指针指向当前这次下一个节点的左节点
-                if(cur.next != null)
-                    cur.right.next = cur.next.left;
+                cur.right.next = cur.next.left;
                 cur = cur.next;
             }
-            // 移动到下一层的第一个节点
-            cur = head;
-            head = null;
+            else
+            {
+                // 如果到了这一层的最后一个节点，把cur指向下一层的第一个节点
+                cur = nextLevel;
+                // 更新下一层的第一个节点
+                nextLevel = cur.left;
+            }
         }
     }
 }
