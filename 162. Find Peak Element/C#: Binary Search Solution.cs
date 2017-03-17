8abea73a2670866a009315abaff85d350dc20974
Hannibal Chen <hannibalcjh@gmail.com>
2017-03-17 16:27:43 -0500

    Update C#: Binary Search Solution.cs

--- 162. Find Peak Element/C#: Binary Search Solution.cs
+++ 162. Find Peak Element/C#: Binary Search Solution.cs
@@ -2,16 +2,20 @@ public class Solution {
     public int FindPeakElement(int[] nums) 
     {
         int left = 0, right = nums.Length - 1;
-        while(left < right)
+        while(left <= right)
         {
-            int mid = left + (right - left) / 2;
-            // 左右指针相邻
-            if(left == mid)
-                return nums[left] < nums[right] ? right : left;
+            if(left == right)
+                return left;
+                
+            int mid = (left + right) / 2;
             if(nums[mid] < nums[mid + 1])
-                left = mid;
-            if(nums[mid] > nums[mid + 1])
+            {
+                left = mid + 1;
+            }
+            else
+            {
                 right = mid;
+            }
         }
         return 0;
     }
