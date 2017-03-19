d4621d69ceb07337645aa701c4f6ffe2eed78c60
Hannibal Chen <hannibalcjh@gmail.com>
2017-03-19 13:41:19 -0500

    Update Java: Solution.java

--- 348. Design Tic-Tac-Toe/Java: Solution.java
+++ 348. Design Tic-Tac-Toe/Java: Solution.java
@@ -5,7 +5,7 @@ public class TicTacToe {
     int[] diag;
     int[] anti_diag;
     int n;
-
+    
     // 时间复杂度O(1)，空间复杂度O(n)，棋盘行和列的数目都为n
     /** Initialize your data structure here. */
     public TicTacToe(int n) 
@@ -17,8 +17,7 @@ public class TicTacToe {
         this.n = n;
     }
     
-    
-    // 时间复杂度O(n^2)，空间复杂度O(n)，棋盘行和列的数目都为n
+    // 时间复杂度O(1)，空间复杂度O(n)，棋盘行和列的数目都为n
     /** Player {player} makes a move at ({row}, {col}).
         @param row The row of the board.
         @param col The column of the board.
@@ -31,7 +30,7 @@ public class TicTacToe {
     {
         // 数组从0开始，使玩家号和下标对应
         player--;
-        // 对应行和列上的棋子个数加1
+        // 下棋，对应行和列上的棋子个数加1
         rows[player][row]++;
         cols[player][col]++;
         // 棋子在对角线上
@@ -40,22 +39,13 @@ public class TicTacToe {
         if(row == n - 1 - col)
             anti_diag[player]++;
         
-        // 检查是否已经有行、列或者对角线被某一个玩家占满
-        for(int i = 0; i < 2; i++)
-        {
-            // 检查对角线
-            if(diag[i] == n || anti_diag[i] == n)
-                return i + 1;
-            for(int j = 0; j < n; j++)
-            {
-                // 检查行
-                if(rows[i][j] == n)
-                    return i + 1;
-                // 检查列
-                if(cols[i][j] == n)
-                    return i + 1;
-            }
-        }
+        // 下完这一步棋以后检查这个玩家是不是已经达到了胜利条件
+        // 在这颗棋子落下后，所在的行或列中该名玩家的棋子是否达到n
+        if(rows[player][row] == n || cols[player][col] == n)
+            return player + 1;
+        // 所在的对角线上该名玩家的棋子是否达到n
+        if(diag[player] == n || anti_diag[player] == n)
+            return player + 1;
         return 0;
     }
 }
