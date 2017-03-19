244e4b964134b7d16a84aea62ad3b61ca44d8f83
Hannibal Chen <hannibalcjh@gmail.com>
2017-03-19 13:45:28 -0500

    Update C#: Solution.cs

--- 348. Design Tic-Tac-Toe/C#: Solution.cs
+++ 348. Design Tic-Tac-Toe/C#: Solution.cs
@@ -5,7 +5,6 @@ public class TicTacToe {
     int[] anti_diag;
     int n;
 
-    // 时间复杂度O(1)，空间复杂度O(n)
     /** Initialize your data structure here. */
     public TicTacToe(int n) 
     {
@@ -16,7 +15,6 @@ public class TicTacToe {
         this.n = n;
     }
     
-    // 时间复杂度O(n^2)，空间复杂度O(n)
     /** Player {player} makes a move at ({row}, {col}).
         @param row The row of the board.
         @param col The column of the board.
@@ -35,19 +33,10 @@ public class TicTacToe {
         if(row == n - 1 - col)
             anti_diag[player]++;
         
-        for(int i = 0; i < 2; i++)
-        {
-            if(diag[i] == n || anti_diag[i] == n)
-                return i + 1;
-            
-            for(int j = 0; j < n; j++)
-            {
-                if(rows[i, j] == n)
-                    return i + 1;
-                if(cols[i, j] == n)
-                    return i + 1;
-            }
-        }
+        if(rows[player, row] == n || cols[player, col] == n)
+            return player + 1;
+        if(diag[player] == n || anti_diag[player] == n)
+            return player + 1;
         return 0;
     }
 }
