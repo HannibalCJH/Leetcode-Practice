3ddcd271dc74149ee2643b88bd9394b06921c8eb
Hannibal Chen <hannibalcjh@gmail.com>
2017-04-04 22:43:48 -0500

    Update Java: Solution.java

--- 275. H-Index II/Java: Solution.java
+++ 275. H-Index II/Java: Solution.java
@@ -1,22 +1,22 @@
 public class Solution {
     // 时间复杂度O(logn)，空间复杂度O(1)
     public int hIndex(int[] citations) 
+    {
+	int n = citations.length;
+	int left = 0, right = citations.length - 1;
+	while(left <= right)
 	{
-		int n = citations.length;
-		int left = 0, right = citations.length - 1;
-		while(left <= right)
-		{
-			int mid = left + (right - left) / 2;
-			// 有n-mid篇论文引用次数大于citations[mid]，说明citations[mid]还可以更大一点，
-			// 我们要找的是最好citations[mid] == n - mid的情况
-			if(citations[mid] < n - mid)
-				left = mid + 1;
-			else
-			    // citations[mid]太大，没有足够的论文数大于这个引用次数
-				right = mid - 1;
-		}
-		// n - left是left点的H指数
-		// 当跳出循环是left指针在right的右边
-		return n - left;
+	    int mid = left + (right - left) / 2;
+	    // 有n-mid篇论文引用次数大于citations[mid]，说明citations[mid]还可以更大一点，
+	    // 我们要找的是最好citations[mid] == n - mid的情况
+	    if(citations[mid] < n - mid)
+		left = mid + 1;
+	    else
+	    // citations[mid]太大，没有足够的论文数大于这个引用次数
+		right = mid - 1;
 	}
+	// n - left是left点的H指数
+	// 当跳出循环是left指针在right的右边
+	return n - left;
+    }
 }
