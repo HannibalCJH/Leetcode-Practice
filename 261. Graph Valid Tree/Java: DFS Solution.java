adf05d416cbfd9b04082c665f25e362d2d378887
Hannibal Chen <hannibalcjh@gmail.com>
2017-04-04 19:15:50 -0500

    Update Java: DFS Solution.java

--- 261. Graph Valid Tree/Java: DFS Solution.java
+++ 261. Graph Valid Tree/Java: DFS Solution.java
@@ -5,50 +5,47 @@ public class Solution {
     // 空间复杂度O(n)
     public boolean validTree(int n, int[][] edges) 
     {
-        int[] visited = new int[n];
+        if(edges.length != n - 1)
+            return false;
         // 邻接链表存节点
-        List<List<Integer>> adjList = new ArrayList<>();
+        List<Integer>[] adjList = new List[n];
         // 初始化每个节点
-        for(int i = 0; i < n; i++) 
-            adjList.add(new ArrayList<Integer>());
+        for(int i = 0; i < n; i++)
+            adjList[i] = new ArrayList<Integer>();
         // 遍历所有的边，把每个节点的邻居节点加入这个节点的链表
         // 注意是无向图，两个方向都加
-        for(int[] edge: edges) 
+        for(int[] edge : edges)
         {
-            adjList.get(edge[0]).add(edge[1]);
-            adjList.get(edge[1]).add(edge[0]);
+            adjList[edge[0]].add(edge[1]);
+            adjList[edge[1]].add(edge[0]);
         }
+        
+        boolean[] visited = new boolean[n];
         // 有环
-        if(hasCycle(-1, 0, visited, adjList)) 
+        if(hasCycle(adjList, -1, 0, visited))
             return false;
         // 检查是否有没被访问过的节点，如果有则不止一个连通部分
-        for(int v : visited) 
-        { 
-            if(v == 0)
+        for(int i = 0; i < n; i++)
+            if(!visited[i])
                 return false;
-        }
         return true;
     }
     
-    private boolean hasCycle(int pred, int vertex, int[] visited, List<List<Integer>> adjList)
+    private boolean hasCycle(List<Integer>[] adjList, int pre, int cur, boolean[] visited)
     {
+        List<Integer> nextList = adjList[cur];
         // 当前节点被访问，设为1
-        visited[vertex] = 1;
+        visited[cur] = true;
         // 遍历当前节点的邻节点
-        for(Integer neighbor: adjList.get(vertex)) 
+        for(int next : nextList)
         {
             // 排除当前节点的前置节点，因为是无向图，所以要避免往回走
-            if(neighbor != pred) 
-            {  
-                // 发现已经被访问过的节点，形成了环
-                if(visited[neighbor] == 1) 
+            if(next != pre)
+            {
+                // 1. 发现已经被访问过的节点，形成了环
+                // 2. 对未被访问过的节点进行递归
+                if(visited[next] || hasCycle(adjList, cur, next, visited))
                     return true;
-                // 对未被访问过的节点进行递归
-                else if(visited[neighbor] == 0) 
-                {
-                    if(hasCycle(vertex, neighbor, visited, adjList)) 
-                        return true;
-                }
             }
         }
         return false;
