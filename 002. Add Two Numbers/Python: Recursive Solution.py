1f129e7970eeec9301df538289daf7b3f11dfac7
Hannibal Chen <hannibalcjh@gmail.com>
2017-05-05 22:36:14 -0500

    Create Python: Recursive Solution.py

---
+++ 002. Add Two Numbers/Python: Recursive Solution.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        def addNumbers(list1, list2, carry):
+            if not list1 and not list2:
+                if carry:
+                    return ListNode(1)
+                else:
+                    return None
+                
+            if not list1:
+                list1 = ListNode(0)
+            if not list2:
+                list2 = ListNode(0)
+                
+            sum = list1.val + list2.val + carry
+            node = ListNode(sum % 10)
+            node.next = addNumbers(list1.next, list2.next, sum // 10)
+            return node
+            
+        return addNumbers(l1, l2, 0)
