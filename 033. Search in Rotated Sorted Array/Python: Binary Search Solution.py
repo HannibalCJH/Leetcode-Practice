eb529a9efeea667021414fd3c45873dacc7a1082
Hannibal Chen <hannibalcjh@gmail.com>
2017-08-31 18:55:10 -0500

    Update Python: Binary Search Solution.py

--- 033. Search in Rotated Sorted Array/Python: Binary Search Solution.py
+++ 033. Search in Rotated Sorted Array/Python: Binary Search Solution.py
@@ -15,13 +15,14 @@ class Solution(object):
                 return mid
             
             if nums[left] <= nums[mid]:
-                if nums[left] <= target and target < nums[mid]:
+                if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
                     left = mid + 1
             else:
-                if nums[mid] < target and target <= nums[right]:
+                if nums[mid] < target <= nums[right]:
                     left = mid + 1
                 else:
-                    right = mid - 1 
+                    right = mid - 1
+        
         return -1
